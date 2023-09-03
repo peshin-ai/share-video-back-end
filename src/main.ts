1ead4b0588bb486baf4a9d286c568be20c3a5bc6
Tuan Ung <t00aienl@gmail.com>
2023-09-04 00:16:37 +0700

    fix: enable cors for port 3000

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   //TODO: remove cors in production
-  app.enableCors();
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    allowedHeaders: '*',
+  });
   await app.listen(5000);
 }
 bootstrap();
